d92799247ad4f835d49eeec6ffd28a4d22da8f57
AsenUzunov <transmit1988@abv.bg>
2022-11-04 14:29:00 +0200

    Cteate table and seed DB

---
+++ HouseRentingSystem.Infrastructure/Data/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class Category
+    {
+        public Category()
+        {
+            Houses = new List<House>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public List<House> Houses { get; set; }
+    }
+}
